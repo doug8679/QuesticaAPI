b2af4dfcd4bea86e22f60cb0feb7f7da82a4fcab
Doug Braxton <Doug@deltasigmacompany.com>
2018-05-08 13:07:47 -0400

    Add project files.

---
+++ QuesticaAPI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuesticaAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
