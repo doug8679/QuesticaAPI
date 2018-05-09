f24f8d08e86840e8e6ed2bd361cea235c5f41b94
Doug Braxton <Doug@deltasigmacompany.com>
2018-05-09 16:21:56 -0400

    Enable CORS

--- QuesticaAPI/Bootstrapper.cs
+++ QuesticaAPI/Bootstrapper.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Nancy;
+using Nancy.Bootstrapper;
 using Nancy.Configuration;
 using Nancy.TinyIoc;
 
@@ -25,6 +26,18 @@ namespace QuesticaAPI
         {
             base.ConfigureApplicationContainer(container);
             container.Register(_serviceProvider.GetService<ILoggerFactory>());
+        }
+
+        protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+        {
+            //CORS Enable
+            pipelines.AfterRequest.AddItemToEndOfPipeline((ctx) =>
+            {
+                ctx.Response.WithHeader("Access-Control-Allow-Origin", "*")
+                    .WithHeader("Access-Control-Allow-Methods", "POST,GET")
+                    .WithHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-type");
+
+            });
         }
     }
 }
