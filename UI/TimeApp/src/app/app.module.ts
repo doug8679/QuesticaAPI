434a2bbc4397092b32f9f380cb4ac35fbe25c77b
Doug Braxton <Doug@deltasigmacompany.com>
2018-05-09 16:22:57 -0400

    Update module to provide all services

--- UI/TimeApp/src/app/app.module.ts
+++ UI/TimeApp/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ObjectivesService } from './objectives.service';
+import { ProjectsService } from './projects.service';
+import { TimecardService } from './timecard.service';
+import { TimeentryService } from './timeentry.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ObjectivesService,
+    ProjectsService,
+    TimecardService,
+    TimeentryService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
