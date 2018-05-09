60b05ffa06b96c5591ce34b2d3b1e015e5234635
Doug Braxton <Doug@deltasigmacompany.com>
2018-05-09 15:45:59 -0400

    Move TimeCardModule to its own file

---
+++ QuesticaAPI/Modules/Api/TimeCardModule.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nancy.ModelBinding;
+using Questica.Model;
+using QuesticaAPI.Models;
+using QuesticaAPI.Responses;
+
+namespace QuesticaAPI.Modules.Api
+{
+    public class TimeCardModule : BaseApiModule
+    {
+        public TimeCardModule() : base("/time")
+        {
+            Get("/{employeeId}", p=> ListTimeEntries(p.employeeId));
+            Post("/entry", p=> UpdateTimeEntry());
+            Put("/entry", p=> PutTimeEntry());
+            Delete("/entry/{timeId}", p=> DeleteTimeEntry(p.timeId));
+        }
+
+        private object DeleteTimeEntry(int timeId)
+        {
+            Console.WriteLine($"Deleting time entry for id ({timeId})...");
+            return new EmptyResponse();
+        }
+
+        private object ListTimeEntries(int employeeId)
+        {
+            using (var db = new TimeData())
+            {
+                var times = db.TimeCardEntries.Include("TimeCard").Where(e => e.EmployeeID.Equals(employeeId)).ToList();
+                var cards = times.Select(e => e.TimeCard).Where(c=>c.PayPeriodStartDate<=DateTime.Now && c.PayPeriodEndDate>=DateTime.Now).Distinct();
+                var result = new List<TimeCardModel>();
+                foreach (var timeCard in cards)
+                {
+                    result.Add(timeCard);
+                }
+
+                return result;
+            }
+        }
+
+        private object PutTimeEntry()
+        {
+            var entry = this.Bind<TimeCardEntryModel>();
+            Console.WriteLine($"Creating entry on project {entry.ProjectID}::{entry.SpecID} for {entry.HourTime} hours.");
+            return new EmptyResponse();
+        }
+
+        private object UpdateTimeEntry()
+        {
+            var entry = this.Bind<TimeCardEntryModel>();
+            Console.WriteLine($"Updatein entry on project {entry.ProjectID}::{entry.SpecID} from {entry.TimeDate} to {entry.HourTime} hours.");
+            return new EmptyResponse();
+        }
+    }
+}
